2cdbc2c953447ff965b39c1509cc315cdf908f30
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2021-07-06 16:12:05 +0100

    create function that takes a number as a parameter and returns the square root of that number

--- question-6/js/script.js
+++ question-6/js/script.js
@@ -1,3 +1,5 @@
+// a function that takes two numbers and returns the sum of the number.
+
 function addTwoNumbers(){
     let num1 = 2;
     let num2 = 50;
@@ -5,3 +7,12 @@ function addTwoNumbers(){
     console.log(" The Sum is: " + sum);
 }
 addTwoNumbers();
+
+/*a function that takes a number as a parameter and 
+returns the square root of that number*/
+
+let isSquare = function(n) {
+   Math.sqrt(n);
+}
+isSquare(console.log(Math.sqrt(4)));
+
